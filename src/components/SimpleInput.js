7cc8b3dc4cbb694a6a19460b01f24e2c608192b2
xXnielsXx <nielsmichael@comcast.net>
2022-08-21 19:22:12 +0900

    Add email input w/ validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,31 +2,69 @@ import { useState } from 'react'
 
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState('')
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
   const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+  const [enteredEmail, setEnteredEmail] = useState('')
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false)
+
+  // inferred state for boolean isValid. Validity not managed via state
+  const enteredNameIsValid = enteredName.trim() !== ''
+
+  const enteredEmailIsValid =
+    // eslint-disable-next-line
+    enteredEmail.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) &&
+    enteredEmail.trim() !== ''
+
+  // boolean var to check if both enteredname is invalid and the entered name was touched by user
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched
+
+  let formIsValid = false
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    // Add more boolean checks w/ && for every form input that gets added
+    // If all are valid whole form is valid
+    formIsValid = true
+  }
 
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value)
   }
 
-  const formSubmitHandler = (e) => {
-    e.preventDefault()
-
-    // On submit, all inputs are considered "touched"
-    setEnteredNameTouched(true)
-
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false)
-      return
-    }
-    setEnteredNameIsValid(true)
-    console.log(enteredName)
+  const emailInputChangeHandler = (e) => {
+    setEnteredEmail(e.target.value)
   }
 
-  // boolean var to check if both enteredname is invalid and the entered name was touched by user
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+  const nameInputBlurHandler = (e) => {
+    // name was touched
+    setEnteredNameTouched(true)
+  }
+
+  const emailInputBlurHandler = (e) => {
+    setEnteredEmailTouched(true)
+  }
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault()
+    // On submit, all inputs are considered "touched"
+    setEnteredNameTouched(true)
+    setEnteredEmailTouched(true)
+
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
+      return
+    }
+    // reset name and touched states
+    setEnteredName('')
+    setEnteredNameTouched(false)
+    setEnteredEmail('')
+    setEnteredEmailTouched(false)
+  }
 
   const nameInputClasses = nameInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control'
+
+  const emailInputClasses = emailInputIsInvalid
     ? 'form-control invalid'
     : 'form-control'
 
@@ -38,14 +76,28 @@ const SimpleInput = () => {
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
         {nameInputIsInvalid && (
           <p style={{ color: 'red' }}>Name must not be empty</p>
         )}
       </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          value={enteredEmail}
+        />
+        {emailInputIsInvalid && (
+          <p style={{ color: 'red' }}>Email must be valid email</p>
+        )}
+      </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   )
